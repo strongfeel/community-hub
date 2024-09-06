a5b82ff75dc9353909928954ea2c599e2a04d1b6
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2024-09-06 13:00:53 +0900

    댓글 작성 API 구현

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express, { Router } from "express";
 import cookieParser from "cookie-parser";
 import UsersRouter from "./routes/users.routers.js";
 import PostsRouter from "./routes/posts.router.js";
+import CommentsRouter from "./routes/comments.router.js";
 import logMiddleware from "./middlewares/log.middleware.js";
 import errorHandlingMiddleware from "./middlewares/error-handling.middleware.js";
 
@@ -12,7 +13,7 @@ app.use(logMiddleware);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", [UsersRouter, PostsRouter]);
+app.use("/api", [UsersRouter, PostsRouter, CommentsRouter]);
 app.use(errorHandlingMiddleware);
 
 app.listen(PORT, () => {
