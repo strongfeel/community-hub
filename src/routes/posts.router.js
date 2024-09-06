3fec4a8e1da6c8eaa0509394984ccabbe59b8e75
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2024-09-06 11:14:42 +0900

    게시글 목록 & 상세 조회 API 구현

--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -21,4 +21,41 @@ router.post("/posts", authMiddleware, async (req, res, next) => {
   return res.status(201).json({ data: post });
 });
 
+// 게시글 목록 조회 API 구현
+router.get("/posts", async (req, res, next) => {
+  const posts = await prisma.posts.findMany({
+    select: {
+      postId: true,
+      title: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+    orderBy: {
+      createdAt: "desc", // 게시글을 최신순으로 정렬
+    },
+  });
+
+  return res.status(200).json({ data: posts });
+});
+
+//게시글 상세 조회 API 구현
+router.get("/posts/:postId", async (req, res, next) => {
+  const { postId } = req.params;
+  const post = await prisma.posts.findFirst({
+    where: {
+      postId: +postId,
+    },
+    select: {
+      postId: true,
+      userId: true,
+      title: true,
+      content: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  return res.status(200).json({ data: post });
+});
+
 export default router;
