497f94b8ef515a5958cf0b82217f73ecd92a1632
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2024-09-06 13:19:07 +0900

    댓글 조회 API 구현

--- src/routes/comments.router.js
+++ src/routes/comments.router.js
@@ -4,6 +4,7 @@ import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
+// 댓글 작성 API 구현
 router.post(
   "/posts/:postId/comments",
   authMiddleware,
@@ -27,5 +28,29 @@ router.post(
     return res.status(201).json({ data: comment });
   }
 );
+
+// 댓글 조회 API
+router.get("/posts/:postId/comments", async (req, res, next) => {
+  const { postId } = req.params;
+
+  const post = await prisma.posts.findFirst({
+    where: {
+      postId: +postId,
+    },
+  });
+  if (!post)
+    return res.status(404).json({ message: "게시글이 존재하지 않습니다." });
+
+  const comments = await prisma.comments.findMany({
+    where: {
+      postId: +postId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return res.status(200).json({ data: comments });
+});
 
 export default router;
